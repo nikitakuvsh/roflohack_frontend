074f05dfca06de52db75e5ef792005c6373b1c67
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2025-04-01 13:29:05 +0300

    fix styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
 import './Header.css';
+import headerLogo from '../../images/header-logo.png';
 
 export default function Header() {
     return (
         <div className='самый_вверх' style={{ backgroundColor: 'yellow', padding: '10px' }}>
-            <nav style={{ display: 'flex', justifyContent: 'center' }}>
-                <div className="меню">
-                    <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold' }}>Главная</li>
-                    <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold' }}>Получить справку</li>
-                    <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold' }}>Контакты</li>
+            <div className='flex'>
+                <div>
+                    <img className='header__img' alt='' src={headerLogo} title='Логотип' />
                 </div>
-            </nav>
+
+                <div>
+                    <nav style={{ display: 'flex', justifyContent: 'center' }}>
+                        <div className="меню">
+                            <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold' }}>Главная</li>
+                            <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold', cursor: 'default' }}>Получить справку</li>
+                            <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold' }}>Контакты</li>
+                            <li style={{ listStyle: 'none', display: 'inline', margin: '0 15px', fontWeight: 'bold', fontStyle: 'italic'}} className='lk--button' >Личный кабинет</li>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </div>
     );
 }
